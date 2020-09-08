2e955c68a3c35d3b0caa2403a03463b2f954ec6e
Chummier <62125658+Chummier@users.noreply.github.com>
2020-09-08 13:23:48 -0700

    Add files via upload

---
+++ FreeroamSystem.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
+
+public class FreeroamSystem : MonoBehaviour {
+
+	public GameObject freeroamLocation;
+	private bool isFreeroam;
+
+	public GameObject startLocation;
+	public GameObject teleFlag;
+	public GameObject teleLocation;
+
+	public GameObject player;
+
+    public GameObject progBarTitle;
+    public Slider progressSlider;
+    private bool progBarFlag;
+
+    private GameObject[] rotators;
+    private Quaternion[] rotations;
+
+    private GameObject[] coins;
+    private GameObject[] stopwatches;
+
+    private bool isResetting;
+    private bool isTeleporting;
+
+    public AudioClip[] backgroundMusic;
+
+    private float timeForNextMusic;
+
+    // Use this for initialization
+    void Start () {
+
+        isFreeroam = false;
+        progBarFlag = false;
+        isResetting = false;
+        isTeleporting = false;
+        timeForNextMusic = 0;
+
+        try
+        {
+            teleFlag.transform.position = startLocation.transform.position;
+            teleFlag.SetActive(false);
+
+            progBarTitle.GetComponent<Text>().text = "";
+            progressSlider.gameObject.SetActive(false);
+            progressSlider.value = 0;
+
+            rotators = GameObject.FindGameObjectsWithTag("Rotating");
+            rotations = new Quaternion[rotators.Length];
+
+            for (int i = 0; i < rotators.Length; i++)
+            {
+                rotations[i] = rotators[i].transform.rotation;
+            }
+
+            coins = GameObject.FindGameObjectsWithTag("Coin");
+            stopwatches = GameObject.FindGameObjectsWithTag("Stopwatch");
+
+            for (int i = 0; i < stopwatches.Length; i++)
+            {
+                stopwatches[i].SetActive(false);
+            }
+        } catch (Exception e)
+        {
+            Debug.Log("Zoinks, FreeroamSystem had an error in Start():\n" + e);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isFreeroam)
+        {
+            if (Time.unscaledTime >= timeForNextMusic)
+            {
+                playMusic();
+            }
+        }
+
+        if (isResetting)
+        {
+            if (progressSlider.value == 100)
+            {
+                progressSlider.gameObject.SetActive(false);
+                progBarTitle.GetComponent<Text>().text = "";
+                progressSlider.value = 0;
+                resetMap(true);
+                isResetting = false;
+            } else
+            {
+                progressSlider.gameObject.SetActive(true);
+                progBarTitle.GetComponent<Text>().text = "Resetting Map...";
+                progressSlider.value += 2;
+            }
+        }
+
+        if (isTeleporting)
+        {
+            if (progressSlider.value == 100)
+            {
+                progressSlider.gameObject.SetActive(false);
+                progBarTitle.GetComponent<Text>().text = "";
+                progressSlider.value = 0;
+
+                teleport();
+                player.GetComponent<MyFPSController>().toggleHint("", 0, false);
+                player.GetComponent<MyFPSController>().updateCoinText(0);
+                isTeleporting = false;
+
+            } else
+            {
+                progressSlider.gameObject.SetActive(true);
+                progBarTitle.GetComponent<Text>().text = "Teleporting...";
+                progressSlider.value += 2;
+            }
+        }
+    }
+
+    public void checkpointInput(InputAction.CallbackContext context)
+    {
+        if (isFreeroam)
+        {
+            placeCheckpoint();
+        }
+    }
+
+    public void resetInput(InputAction.CallbackContext context)
+    {
+        if (isFreeroam && context.ReadValueAsButton() && !isTeleporting)
+        {
+            isResetting = true;
+        } else
+        {
+            if (!isTeleporting)
+            {
+                progressSlider.gameObject.SetActive(false);
+                progBarTitle.GetComponent<Text>().text = "";
+                progressSlider.value = 0;
+            }
+            isResetting = false;
+        }
+    }
+
+    public void teleportInput(InputAction.CallbackContext context)
+    {
+        if (isFreeroam && context.ReadValueAsButton() && !isResetting)
+        {
+            isTeleporting = true;
+        } else
+        {
+            if (!isResetting)
+            {
+                progressSlider.gameObject.SetActive(false);
+                progBarTitle.GetComponent<Text>().text = "";
+                progressSlider.value = 0;
+            }
+            isTeleporting = false;
+        }
+    }
+
+	private void OnTriggerEnter(Collider c){
+		
+		if (c.gameObject.CompareTag ("Player")) {
+
+            isFreeroam = true;
+
+            try
+            {
+                teleFlag.SetActive(true);
+                teleFlag.transform.position = freeroamLocation.transform.position;
+                teleLocation.transform.position = freeroamLocation.transform.position;
+
+                player.GetComponent<MyFPSController>().setDoubleJumpRecharge(true);
+                player.GetComponent<MyFPSController>().rechargeToMaxSprint();
+
+                player.GetComponent<MyFPSController>().toggleHint("Hold T to return to the bus", 10, true);
+                player.GetComponent<MyFPSController>().playerHUD.SetActive(true);
+
+                playMusic();
+
+            } catch (Exception e)
+            {
+                Debug.Log("My my, an error in FreeroamSystem in OnTriggerEnter():\n" + e);
+            }
+
+		}
+	}
+
+    public void playMusic()
+    {
+        if (isFreeroam)
+        {
+            int index = (int)Random.Range(0, backgroundMusic.Length - 1);
+            player.GetComponent<AudioSource>().PlayOneShot(backgroundMusic[index], PlayerPrefs.GetFloat("MusicVolume", 1f));
+
+            timeForNextMusic = Time.time + backgroundMusic[index].length + 10f;
+        }
+    }
+
+	public bool getFreeroamStatus(){
+		return isFreeroam;
+	}
+
+    public void setIsFreeroam(bool flag)
+    {
+        isFreeroam = flag;
+    }
+
+    public void placeCheckpoint(){
+        if (player.GetComponent<MyFPSController>().isGrounded() && isFreeroam)
+        {
+            teleLocation.transform.position = player.transform.position;
+            teleFlag.transform.position = player.transform.position;
+        }
+	}
+
+    public void resetMap(bool coinsStopwatches)
+    {
+        resetRotatingObjects();
+        toggleCoinsStopwatches(coinsStopwatches);
+    }
+
+    public void teleport()
+    {
+        isFreeroam = false;
+
+        toSpawn();
+        resetMap(true);
+
+        teleFlag.transform.position = startLocation.transform.position;
+        teleFlag.SetActive(false);
+
+        player.GetComponent<MyFPSController>().toggleHint("", 0, false);
+
+        player.GetComponent<MyFPSController>().rechargeToMaxSprint();
+
+        player.GetComponent<AudioSource>().Stop();
+    }
+
+    public void resetRotatingObjects()
+    {
+        for (int i = 0; i < rotators.Length; i++)
+        {
+            rotators[i].transform.rotation = rotations[i];
+            rotators[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+    }
+
+    public void toggleCoinsStopwatches(bool flag)
+    {
+        player.GetComponent<MyFPSController>().coinCount = 0;
+        player.GetComponent<MyFPSController>().updateCoinText(0);
+        for (int i = 0; i < coins.Length; i++)
+        {
+            coins[i].SetActive(flag);
+            stopwatches[i].SetActive(!flag);
+        }
+    }
+
+    public void toSpawn()
+    {
+        player.transform.position = startLocation.transform.position;
+        player.transform.rotation = startLocation.transform.rotation;
+    }
+
+    public void toFlag()
+    {
+        player.transform.position = teleLocation.transform.position;
+        player.transform.rotation = teleLocation.transform.rotation;
+    }
+}
---
+++ HighscoreSystem.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public struct RaceHighscore
+{
+    public string name;
+    public float time;
+
+    public RaceHighscore(string username, float seconds)
+    {
+        name = username;
+        time = seconds;
+    }
+    
+}
+[System.Serializable]
+public struct RaceHighscoreList
+{
+    public List<RaceHighscore> list;
+
+    public RaceHighscoreList(int length)
+    {
+        list = new List<RaceHighscore>(length);
+        for (int i = 0; i < length; i++)
+        {
+            list.Add(new RaceHighscore("None", 00.00f));
+        }
+    }
+}
+[System.Serializable]
+public struct CoinHighscore
+{
+    public string name;
+    public int coins;
+
+    public CoinHighscore(string username, int amount)
+    {
+        name = username;
+        coins = amount;
+    }
+}
+[System.Serializable]
+public struct CoinHighscoreList
+{
+    public List<CoinHighscore> list;
+
+    public CoinHighscoreList(int length)
+    {
+        list = new List<CoinHighscore>(length);
+        for (int i = 0; i < length; i++)
+        {
+            list.Add(new CoinHighscore("None", 0));
+        }
+    }
+}
+
+[System.Serializable]
+public class Data
+{
+    public Dictionary<int, CoinHighscoreList> localCoin;
+    public Dictionary<int, RaceHighscoreList> localRace;
+    public Dictionary<int, CoinHighscoreList> devCoin;
+    public Dictionary<int, RaceHighscoreList> devRace;
+
+    public Data(Dictionary<int, CoinHighscoreList> coinDict, Dictionary<int, RaceHighscoreList> raceDict)
+    {
+        localCoin = coinDict;
+        localRace = raceDict;
+    }
+
+    public Data(Dictionary<int, CoinHighscoreList> coinDict, Dictionary<int, RaceHighscoreList> raceDict, Dictionary<int, CoinHighscoreList> coinDev, Dictionary<int, RaceHighscoreList> raceDev)
+    {
+        localCoin = coinDict;
+        localRace = raceDict;
+        devCoin = coinDev;
+        devRace = raceDev;
+    }
+}
+
+public class HighscoreSystem : MonoBehaviour
+{
+    private static int scoresAmount = 10;
+    private GameObject[] CoinScoreSlots = new GameObject[scoresAmount];
+    private GameObject[] CoinDevScoreSlots = new GameObject[scoresAmount / 2];
+
+    private GameObject[] RaceScoreSlots = new GameObject[scoresAmount];
+    private GameObject[] RaceDevScoreSlots = new GameObject[scoresAmount / 2];
+
+    public Dictionary<int, CoinHighscoreList> CoinLevels;
+    public Dictionary<int, RaceHighscoreList> RaceLevels;
+    public Dictionary<int, CoinHighscoreList> DevCoinLevels;
+    public Dictionary<int, RaceHighscoreList> DevRaceLevels;
+
+    public Animator KeysAnimator;
+
+    private float distanceToTV;
+    private bool atTV;
+
+    public GameObject player;
+    public GameObject playerHUD;
+    public GameObject highscoreMenu;
+
+    private float currentLevel;
+    public Text levelIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        atTV = false;
+
+        currentLevel = 0;
+        levelIndicator.text = "Level " + (currentLevel + 1).ToString();
+
+        CoinLevels = new Dictionary<int, CoinHighscoreList>();
+        RaceLevels = new Dictionary<int, RaceHighscoreList>();
+        DevCoinLevels = new Dictionary<int, CoinHighscoreList>();
+        DevRaceLevels = new Dictionary<int, RaceHighscoreList>();
+
+        for (int i = 1; i < 6; i++)
+        {
+            CoinLevels.Add(i, new CoinHighscoreList(scoresAmount));
+            RaceLevels.Add(i, new RaceHighscoreList(scoresAmount));
+            DevCoinLevels.Add(i, new CoinHighscoreList(scoresAmount / 2));
+            DevRaceLevels.Add(i, new RaceHighscoreList(scoresAmount / 2));
+        }
+
+        try
+        {
+            for (int i = 0; i < CoinScoreSlots.Length; i++)
+            {
+                CoinScoreSlots[i] = GameObject.Find("CoinNameScore" + (i + 1).ToString());
+            }
+
+            for (int i = 0; i < RaceScoreSlots.Length; i++)
+            {
+                RaceScoreSlots[i] = GameObject.Find("RaceNameScore" + (i + 1).ToString());
+            }
+
+            for (int i = 0; i < CoinDevScoreSlots.Length; i++)
+            {
+                CoinDevScoreSlots[i] = GameObject.Find("CoinDevScore" + (i + 1).ToString());
+            }
+
+            for (int i = 0; i < RaceDevScoreSlots.Length; i++)
+            {
+                RaceDevScoreSlots[i] = GameObject.Find("RaceDevScore" + (i + 1).ToString());
+            }
+
+            initializeDevScores();
+            loadLocalData();
+            changeLevel(0);
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Dang, error in HighscoreSystem:\n" + e);
+        }
+    }
+
+    private void Update()
+    {
+        if (atTV) {
+            distanceToTV = (highscoreMenu.transform.position - player.transform.position).magnitude;
+            if (distanceToTV >= 3)
+            {
+                atTV = false;
+                playerHUD.SetActive(true);
+            }
+        }
+    }
+
+    private void changeLevel(int level)
+    {
+
+        for (int i = 0; i < CoinScoreSlots.Length; i++)
+        {
+            CoinScoreSlots[i].GetComponent<Text>().text = CoinLevels[level+1].list[i].name + " " + CoinLevels[level+1].list[i].coins.ToString();
+        }
+        for (int i = 0; i < CoinDevScoreSlots.Length; i++)
+        {
+            CoinDevScoreSlots[i].GetComponent<Text>().text = DevCoinLevels[level + 1].list[i].name + " " + DevCoinLevels[level + 1].list[i].coins.ToString();   
+        }
+
+        for (int i = 0; i < RaceScoreSlots.Length; i++)
+        {
+            RaceScoreSlots[i].GetComponent<Text>().text = RaceLevels[level+1].list[i].name + " " + RaceLevels[level+1].list[i].time.ToString("F");
+        }
+        for (int i = 0; i < RaceDevScoreSlots.Length; i++)
+        {
+            RaceDevScoreSlots[i].GetComponent<Text>().text = DevRaceLevels[level + 1].list[i].name + " " + DevRaceLevels[level + 1].list[i].time.ToString("F");
+        }
+    }
+
+    public void levelInput(InputAction.CallbackContext context)
+    {
+        if (atTV)
+        {
+            // Next level
+            if (context.ReadValue<Vector2>().x >= 1)
+            {
+                currentLevel = currentLevel + 0.5f; // Button gets pressed twice for some reason
+                if (currentLevel >= 5)
+                {
+                    currentLevel = 0;
+                }
+                KeysAnimator.SetTrigger("RightTrigger");
+            }
+
+            // Previous level
+            else if (context.ReadValue<Vector2>().x <= -1)
+            {
+                currentLevel = currentLevel - 0.5f; // Button gets pressed twice for some reason
+                if (currentLevel <= -1)
+                {
+                    currentLevel = 4;
+                }
+                KeysAnimator.SetTrigger("LeftTrigger");
+            }
+
+            levelIndicator.text = "Level " + ((int)currentLevel + 1).ToString();
+            changeLevel((int)currentLevel);
+        }
+    }
+
+    public void updateRace(int level, string user, float value)
+    {
+        for (int i = 0; i < scoresAmount; i++)
+        {
+            if (value > RaceLevels[level].list[i].time)
+            {
+                for (int j = scoresAmount-1; j > i; j--)
+                {
+                    RaceLevels[level].list[j] = RaceLevels[level].list[j - 1];
+                }
+                RaceLevels[level].list[i] = new RaceHighscore(user, value);
+                changeLevel(0);
+                return;
+            }
+        }
+    }
+
+    private void updateDevRace(int level, string user, float value)
+    {
+        for (int i = 0; i < scoresAmount/2; i++)
+        {
+            if (value > DevRaceLevels[level].list[i].time)
+            {
+                for (int j = scoresAmount/2 - 1; j > i; j--)
+                {
+                    DevRaceLevels[level].list[j] = DevRaceLevels[level].list[j - 1];
+                }
+                DevRaceLevels[level].list[i] = new RaceHighscore(user, value);
+                return;
+            }
+        }
+    }
+
+    public void updateCoin(int level, string user, int value)
+    {
+        for (int i = 0; i < scoresAmount; i++)
+        {
+            if (value > CoinLevels[level].list[i].coins)
+            {
+                for (int j = scoresAmount - 1; j > i; j--)
+                {
+                    CoinLevels[level].list[j] = CoinLevels[level].list[j - 1];
+                }
+                CoinLevels[level].list[i] = new CoinHighscore(user, value);
+                changeLevel(0);
+                return;
+            }
+        }
+    }
+
+    private void updateDevCoin(int level, string user, int value)
+    {
+        for (int i = 0; i < scoresAmount/2; i++)
+        {
+            if (value > DevCoinLevels[level].list[i].coins)
+            {
+                for (int j = scoresAmount/2 - 1; j > i; j--)
+                {
+                    DevCoinLevels[level].list[j] = DevCoinLevels[level].list[j - 1];
+                }
+                DevCoinLevels[level].list[i] = new CoinHighscore(user, value);
+                return;
+            }
+        }
+    }
+
+    private void initializeDevScores()
+    {
+        updateDevRace(1, "Jared～", 19.64f);
+        updateDevRace(1, "Jared～", 23.80f);
+        updateDevRace(1, "Jared～", 14.42f);
+        updateDevRace(1, "Jared～", 15.22f);
+        updateDevRace(1, "Jared～", 9.62f);
+
+        updateDevRace(2, "Jared～", 17.22f);
+        updateDevRace(2, "Jared～", 12.86f);
+        updateDevRace(2, "Jared～", 12.38f);
+        updateDevRace(2, "Jared～", 17.60f);
+        updateDevRace(2, "Jared～", 11.55f);
+
+        updateDevRace(3, "Jared～", 12.34f);
+        updateDevRace(3, "Jared～", 10.82f);
+        updateDevRace(3, "Jared～", 9.94f);
+        updateDevRace(3, "Jared～", 15.59f);
+        updateDevRace(3, "Jared～", 18.55f);
+
+        updateDevRace(4, "Jared～", 20.82f);
+        updateDevRace(4, "Jared～", 23.74f);
+        updateDevRace(4, "Jared～", 23.84f);
+        updateDevRace(4, "Jared～", 22.61f);
+        updateDevRace(4, "Jared～", 17.12f);
+
+        updateDevRace(5, "Jared～", 1.64f);
+        updateDevRace(5, "Jared～", 2.40f);
+        updateDevRace(5, "Jared～", 2.48f);
+        updateDevRace(5, "Jared～", 3.14f);
+        updateDevRace(5, "Jared～", 2.30f);
+
+        updateDevCoin(1, "Jared～", 7);
+        updateDevCoin(1, "Jared～", 6);
+        updateDevCoin(1, "Jared～", 9);
+        updateDevCoin(1, "Jared～", 8);
+        updateDevCoin(1, "Jared～", 5);
+
+        updateDevCoin(2, "Jared～", 13);
+        updateDevCoin(2, "Jared～", 10);
+        updateDevCoin(2, "Jared～", 11);
+        updateDevCoin(2, "Jared～", 9);
+        updateDevCoin(2, "Jared～", 8);
+
+        updateDevCoin(3, "Jared～", 5);
+        updateDevCoin(3, "Jared～", 7);
+        updateDevCoin(3, "Jared～", 6);
+        updateDevCoin(3, "Jared～", 4);
+        updateDevCoin(3, "Jared～", 9);
+
+        updateDevCoin(4, "Jared～", 6);
+        updateDevCoin(4, "Jared～", 7);
+        updateDevCoin(4, "Jared～", 9);
+        updateDevCoin(4, "Jared～", 5);
+        updateDevCoin(4, "Jared～", 8);
+
+        updateDevCoin(5, "Jared～", 8);
+        updateDevCoin(5, "Jared～", 10);
+        updateDevCoin(5, "Jared～", 13);
+        updateDevCoin(5, "Jared～", 15);
+        updateDevCoin(5, "Jared～", 11);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            atTV = true;
+            playerHUD.SetActive(false);
+            player.GetComponent<PauseMenuScript>().togglePauseMenu(false);
+            loadLocalData();
+        }
+    }
+
+    public void saveLocalData()
+    {
+        string target = Application.persistentDataPath + "/scores.hs";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file;
+
+        if (File.Exists(target))
+        {
+            file = File.OpenWrite(target);
+        } else
+        {
+            file = File.Create(target);
+        }
+
+        Data data = new Data(CoinLevels, RaceLevels);
+        formatter.Serialize(file, data);
+        file.Close();
+    }
+
+    public void loadLocalData()
+    {
+        string target = Application.persistentDataPath + "/scores.hs";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file;
+
+        if (File.Exists(target))
+        {
+            file = File.OpenRead(target);
+        }
+        else
+        {
+            Debug.Log("No highscores file found");
+            return;
+        }
+
+        Data data = (Data)formatter.Deserialize(file);
+        file.Close();
+
+        CoinLevels = data.localCoin;
+        RaceLevels = data.localRace;
+    }
+}
